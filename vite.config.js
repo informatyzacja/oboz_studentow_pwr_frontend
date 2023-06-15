dc773a882ba7871c2f05640e4c648622cac4c82f
Marvin Ruciński <marvin@prasa-polska.com>
2023-06-15 20:49:52 +0200

    added pwa

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,27 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), VitePWA({ 
+    manifest: {
+      name: 'Obóz Studentów PWr',
+      short_name: 'Obóz PWR',
+      themeColor: '#de7539',
+      msTileColor: '#000000',
+      orientation: "portrait-primary",
+      lang: 'pl',
+      description: "Aplikacja mobilna Obóz Studentów PWr",
+      appleMobileWebAppCapable: 'yes',
+      appleMobileWebAppStatusBarStyle: 'black-translucent',
+    },
+    registerType: 'autoUpdate'
+   })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
 })
